4a4bda5773cfc1d5a59f4064e66f5db68958150c
jaykim <jayjkim0223@gmail.com>
2025-08-20 20:29:36 +0900

    빌드 오류 수정

--- chatbot-backend/src/main/java/com/incheonai/chatbotbackend/ChatbotBackendApplication.java
+++ chatbot-backend/src/main/java/com/incheonai/chatbotbackend/ChatbotBackendApplication.java
@@ -9,12 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableJpaRepositories(basePackages = "com.incheonai.chatbotbackend.repository.jpa")
-@EnableRedisHttpSession
 @SpringBootApplication
 public class ChatbotBackendApplication {
 	public static void main(String[] args) {
