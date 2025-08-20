8547a6146c93acceeac2f2cc00b8fd551363077e
jaykim <jayjkim0223@gmail.com>
2025-08-20 18:42:05 +0900

    세션 불일치 수정

--- chatbot-backend/src/main/java/com/incheonai/chatbotbackend/ChatbotBackendApplication.java
+++ chatbot-backend/src/main/java/com/incheonai/chatbotbackend/ChatbotBackendApplication.java
@@ -11,9 +11,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableJpaRepositories(basePackages = "com.incheonai.chatbotbackend.repository.jpa")
-@EnableMongoRepositories(basePackages = "com.incheonai.chatbotbackend.repository.mongodb")
+@EnableMongoRepositories(basePackages = "com.incheonai.chatbotbackend.repository.primary")
+@EnableRedisHttpSession
 @SpringBootApplication
 public class ChatbotBackendApplication {
 	public static void main(String[] args) {
